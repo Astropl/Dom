8f174615193c885b927b35be43e0faa6e3c4eb10
Astropl <30636834+Astropl@users.noreply.github.com>
2018-01-20 23:57:36 +0100

    Add files via upload

--- CheckTime.java
+++ CheckTime.java
@@ -2,12 +2,27 @@ package Dom;
 
 import java.time.LocalDateTime;
 
+import static java.time.LocalDateTime.*;
+
 public class CheckTime extends Thread
 
-{public void CheckStart(int czas1)
-    {
-       LocalDateTime a = LocalDateTime.now();
-        System.out.println(Format);
+{
+    public Object minus;
+
+    public CheckTime(LocalDateTime czas) {
+        LocalDateTime a = now();
+        System.out.println(a);
+
+        return;
 
     }
-}
+
+    /*public CheckTime(LocalDateTime czas2) {
+        LocalDateTime b = now();
+        System.out.println(b);
+        return;*/
+
+
+    }
+
+
--- Main.java
+++ Main.java
@@ -1,27 +1,48 @@
 package Dom;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        CheckTime checkTime = new CheckTime();
+        LocalDateTime a = null;
+        CheckTime checkTime = new CheckTime(LocalDateTime.now());
 
         Scanner sc = new Scanner(System.in);
         Random gr = new Random();
 
         System.out.println("W losowym czasie  od 1 sekundy do 10 sekund pojawi się napis TERAZ\nTy w tym czasie musisz nacisnąć jak najszybciej klawisz ENTER.\nSprawdze Twój czas reakcji");
-        Timings timings = new Timings(7000);
+        Timings timings = new Timings(2000);
 
         System.out.println(" GOTÓW ? \nNo to zaczynamy");
         int czasowka = gr.nextInt(10);
         //System.out.println(czasowka); // Sprawdzamile wynosi czas oczekiwania na start w randomie
         System.out.println("START");
-        checkTime.CheckStart(0);
+        /*
+        checkTime
+        int czas1;
+        */
+
+        CheckTime zmienna1 = new CheckTime(a);
+        //CheckTime asder = zmienna1;
 
         String klawiszEnter = sc.nextLine();
-        System.out.println(" Twoj czas reakcji to :"+ " ");
 
+        System.out.println(" Twoj czas reakcji to :" + " ");
+        CheckTime zmienna2 = new CheckTime(a);
+
+        // A tak sobie sprawdze dwie zeinne czasowe
+        System.out.println(zmienna1);
+        System.out.println(zmienna2);
+        System.out.println(zmienna2.minus(Duration.zmienna1));
+        
+        
+        /*LocalDate a = LocalDate.of(2014, 6, 30);
+        LocalDate b = a.minus(Period.ofDays(100));
+        System.out.println(b);*/
     }
 }
